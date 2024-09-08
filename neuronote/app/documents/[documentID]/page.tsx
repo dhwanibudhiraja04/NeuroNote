04aed018ed57e73197e558fbe3bf1302fac311be
Sahil Shenoy <sahilshenoy3@gmail.com>
2024-09-08 17:55:31 +0530

    adding skeleton loaders

--- neuronote/app/documents/[documentID]/page.tsx
+++ neuronote/app/documents/[documentID]/page.tsx
@@ -6,6 +6,8 @@ import { useQuery } from "convex/react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { useParams } from "next/navigation";
 import ChatPanel from "./chat-panel";
+import { Skeleton } from "@/components/ui/skeleton";
+
 
 export default function DocumentPage() {
   const { documentID } = useParams();  // Extract the documentID from the URL
@@ -22,36 +24,53 @@ export default function DocumentPage() {
     return <div>Loading...</div>;  // Handle the case where documentId is undefined
   }
 
-  if (!document) {
-    return <div>You do not have access to view this document</div>;
-  }
+  
 
   return (
     <main className="p-24 space-y-8">
-      <div className="flex justify-between items-center">
-        <h1 className="text-4xl font-bold">{document.title}</h1>
-      </div>
+      {!document && (
+        <div className="space-y-8">
+          <div>
+            <Skeleton className="h-[40px] w-[500px]" />
+          </div>
+          <div className="flex gap-2">
+            <Skeleton className="h-[40px] w-[80px]" />
+            <Skeleton className="h-[40px] w-[80px]" />
+          </div>
+          <Skeleton className="h-[500px]" />
+        </div>
+      )}
 
-      <div className="flex gap-12">
+      {document && (
+        <>
+          <div className="flex justify-between items-center">
+            <h1 className="text-4xl font-bold">{document.title}</h1>
+          </div>
 
-        <Tabs defaultValue="document" className="w-full">
-          <TabsList className="mb-2">
-            <TabsTrigger value="document">Document</TabsTrigger>
-            <TabsTrigger value="chat">Chat</TabsTrigger>
-          </TabsList>
+          <div className="flex gap-12">
+            <Tabs defaultValue="document" className="w-full">
+              <TabsList className="mb-2">
+                <TabsTrigger value="document">Document</TabsTrigger>
+                <TabsTrigger value="chat">Chat</TabsTrigger>
+              </TabsList>
 
-        <TabsContent value="document">
-            <div className="bg-gray-900 p-4 rounded-xl flex-1 h-[500px]">
-              {document.documentUrl && (
-                <iframe className="w-full h-full" src={document.documentUrl} />
-              )}
-            </div>
-          </TabsContent>
-          <TabsContent value="chat">
-            <ChatPanel documentId={document._id} />
-          </TabsContent>
-        </Tabs>
-      </div>
+              <TabsContent value="document">
+                <div className="bg-gray-900 p-4 rounded-xl flex-1 h-[500px]">
+                  {document.documentUrl && (
+                    <iframe
+                      className="w-full h-full"
+                      src={document.documentUrl}
+                    />
+                  )}
+                </div>
+              </TabsContent>
+              <TabsContent value="chat">
+                <ChatPanel documentId={document._id} />
+              </TabsContent>
+            </Tabs>
+          </div>
+        </>
+      )}
     </main>
   );
 }
--- neuronote/app/header.tsx
+++ neuronote/app/header.tsx
@@ -2,6 +2,7 @@
 
 import { ModeToggle } from "@/components/ui/mode-toggle";
 import Image from "next/image";
+import Link from "next/link";
 import { HeaderActions } from "@/app/header-actions";
 import { Montserrat } from 'next/font/google'
 
@@ -17,15 +18,23 @@ export function Header() {
   return (
     <div className={`${montserrat.variable} py-4`} style={{ backgroundColor: "#5067AA" }}>
       <div className="container mx-auto flex justify-between items-center">
-        <div className="flex items-center gap-4 text-2xl">
-          <Image
-            src="/favicon.ico"
-            width={40}
-            height={40}
-            className="rounded"
-            alt="an image of a brain in archive"
-          />
-          NeuroNote
+      <div className="flex gap-12 items-center">
+          <Link href="/" className="flex items-center gap-4 text-2xl">
+            <Image
+              src="/favicon.ico"
+              width={40}
+              height={40}
+              className="rounded"
+              alt="an image of a brain"
+            />
+            NeuroNote
+          </Link>
+
+          <nav>
+            <Link href="/" className="hover:text-slate-300">
+              Documents
+            </Link>
+          </nav>
         </div>
         <div className="flex gap-4 items-center">
           <ModeToggle />
